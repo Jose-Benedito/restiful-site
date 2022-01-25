e6585d8550da9f716f16f4ba498e0b5d85d46f7b
jose-benedito <benethowen@yahoo.com.br>
2022-01-25 00:27:59 -0300

    Site inicial para consumi a  restiful-api

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const request = require('request-promise-native')
+
+
+const app = express()
+
+app.set('view engine', 'ejs'); //para acessar o template ejs
+app.set('views', './src/views'); //acessar a pasta views
+
+
+// função assíncrona que pega a requisição da api usando os modulos request e request-promise-native(ver documentação)
+app.get('/produtos', async function(req, res){
+ const result =  await  request.get('http://localhost:3000/api/v1/products');
+ // debug: console.log(JSON.parse(result));
+//passando os dados para a página html
+const products = JSON.parse(result).data;
+
+    res.render("products", {products});
+})
+
+
+
+app.listen(3001, function() {
+    console.log('Servidor inicializado na porta 3001');
+})
