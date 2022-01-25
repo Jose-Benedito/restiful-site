62cb8a7def4f3efb48d0d05d4fee28bed7e4c683
jose-benedito <benethowen@yahoo.com.br>
2022-01-25 01:01:18 -0300

    Incluindo bootstrap no html

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const request = require('request-promise-native')
 
 
 const app = express()
-
+//funções do express
 app.set('view engine', 'ejs'); //para acessar o template ejs
-app.set('views', './src/views'); //acessar a pasta views
-
+app.set('views', './src/views'); //para acessar a pasta views
+app.use(express.static('public'))//para acessar a pasta public com os assets
 
 // função assíncrona que pega a requisição da api usando os modulos request e request-promise-native(ver documentação)
 app.get('/produtos', async function(req, res){
